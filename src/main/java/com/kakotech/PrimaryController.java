75fae97cde640207e6211f08fa599c6b5b40bb05
Speedelek <kakoczkyakos@gmail.com>
2019-08-12 23:03:01 +0200

    working initialize method

--- src/main/java/com/kakotech/PrimaryController.java
+++ src/main/java/com/kakotech/PrimaryController.java
@@ -8,15 +8,19 @@ import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 
-public class PrimaryController {
+public class PrimaryController implements Initializable {
 
+    @FXML
+    TableView tableDictionary;
     @FXML
     Button dictionaryButton;
     @FXML
@@ -63,23 +67,40 @@ public class PrimaryController {
         askPane.setVisible(true);
     }
 
+    /*
     public void setTableData(){
         TableColumn columnEng = new TableColumn("English");
         columnEng.setMinWidth(100);
         columnEng.setCellFactory(TextFieldTableCell.forTableColumn());
         //columnEng.setCellValueFactory(new PropertyValueFactory<Person, String>("lastName"));
-    }
+    }*/
 
     private final ObservableList<Line> data =
             FXCollections.observableArrayList(
-                    new Line("english1", "hungarian1", "example sentence1"),
-                    new Line("english2", "hungarian2", "example sentence2"),
-                    new Line("english3", "hungarian3", "example sentence3")
+                    new Line("absence", "hiány", "A new teacher was appointed during his absence."),
+                    new Line("duke", "herceg", "A duki is a man of very high rank."),
+                    new Line("earl", "gróf", "An earl is a man of high social rank.")
             );
 
     @Override
     public void initialize(URL url, ResourceBundle rb){
         TableColumn columnEng = new TableColumn("English word");
+        columnEng.setMinWidth(100);
+        columnEng.setCellFactory(TextFieldTableCell.forTableColumn());
+        columnEng.setCellValueFactory(new PropertyValueFactory<Line, String>("wordEnglish"));
+
+        TableColumn columnHun = new TableColumn("Hungarian word");
+        columnHun.setMinWidth(100);
+        columnHun.setCellFactory(TextFieldTableCell.forTableColumn());
+        columnHun.setCellValueFactory(new PropertyValueFactory<Line, String>("wordHungarian"));
+
+        TableColumn columnExample = new TableColumn("Example sentence");
+        columnExample.setMinWidth(300);
+        columnExample.setCellFactory(TextFieldTableCell.forTableColumn());
+        columnExample.setCellValueFactory(new PropertyValueFactory<Line, String>("exampleSentence"));
+
+        tableDictionary.getColumns().addAll(columnEng, columnHun, columnExample);
+        tableDictionary.setItems(data);
     }
 
 
